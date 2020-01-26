00fd61a42e5211d15f2688f4f9d3008b799a5813
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-01-26 17:54:51 +0530

    splash: add animate out

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print("inside build");
     return MaterialApp(
       title: 'Placement',
       initialRoute: '/',
