50cdc45d03919a70102f27414fbe409830a18963
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-01-26 18:01:19 +0530

    screens: add basic screen structures

---
+++ lib/screens/authenticate/authenticatePage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:placement/screens/authenticate/signIn.dart';
+import 'package:placement/screens/authenticate/signUp.dart';
+
+
+class Authenticate extends StatefulWidget {
+  Authenticate({Key key}) : super(key: key);
+
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  bool sign_in_shown=true;
+  void toggle() {
+    setState(() => sign_in_shown = !sign_in_shown);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: sign_in_shown? SignIn(toggleview: toggle) : Register(toggleView: toggle),
+    );
+  }
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:placement/screens/authenticate/authenticatePage.dart';
+import 'package:placement/screens/home/homePage.dart';
+import 'package:provider/provider.dart';
+import 'package:placement/models/user.dart';
+
+class WrapperPage extends StatelessWidget {
+  const WrapperPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //final user = Provider.of<User>(context);
+    //return user ==null ? Authenticate() : HomePage();
+    return HomePage();
+  }
+}
