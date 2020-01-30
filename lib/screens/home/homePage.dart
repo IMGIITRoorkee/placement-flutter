de11f5b6d6e3def53c334472034d7f0ae442c9fd
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-01-30 19:41:34 +0530

    update homepage

--- lib/screens/home/homePage.dart
+++ lib/screens/home/homePage.dart
@@ -1,19 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:placement/resources/strings.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+
+  TabController _tabController;
+  dynamic a;
+  
+  final List<Tab> _bottomTab = <Tab>[
+      Tab(
+        icon: Icon(Icons.access_alarm),
+      ),
+      Tab(
+        icon: Icon(Icons.account_box),
+      ),
+      Tab(
+        icon: Icon(Icons.add_circle),
+      ),
+      Tab(
+        icon: Icon(Icons.add_alert),
+      ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      vsync: this,
+      length: 4
+    );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Placement"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              print("Notified!!");
+            },
+          ),
+        ],
+        title: Text(Strings.HOME_APPBAR),
+        centerTitle: true,
       ),
-      body: Container(
-        child: Center(
-          child: Text("Welcome Home!!"),
-        ),
+      body: TabBarView(
+        controller: _tabController,
+        children: _tabSelector(),
+      ),
+      bottomNavigationBar: TabBar(
+        controller: _tabController,
+        tabs: _bottomTab,
+        indicatorPadding: EdgeInsets.all(5.0),
+        labelColor: Colors.red,
       ),
     );
+  }
+
+  _tabSelector() {
+    return <Widget>[
+      Container(
+        child: Text("Under Construction")
+      ),
+      Container(
+        child: Text("Under Construction")
+      ),
+      Container(
+        child: Text("Under Construction")
+      ),
+      Container(
+        child: Text("Under Construction")
+      ),
+    ];
   }
 }
