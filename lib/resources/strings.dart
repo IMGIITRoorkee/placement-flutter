1156bc9d7beeeb216b65f7f5dacb6271e444290e
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-01-30 22:50:42 +0530

    updated strings added

--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -2,6 +2,16 @@ class Strings {
   static const String PLACEMENT = 'Placement Online';
   static const String BRANDING = 'Channel i';
   static const String HOME_APPBAR = 'Internship Season';
+  static const String PLACEMENT_YEAR = 'Placement 2019';
+
+  static const List<String> APPLY_TABBAR = <String>[
+    'Open for Me',
+    'All Companies'
+  ];
+  static const List<String> RESULT_TABBAR = <String>[
+    'By Branch',
+    'By Company'
+  ];
   
   static const String AUTH_BOX = 'placementAuthBox';
 }
