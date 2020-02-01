1896cd07f3829af03c559fedd4a6d621c397dc7f
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-02-01 16:42:32 +0530

    endpoints: add fresh api endpoints

--- lib/resources/endpoints.dart
+++ lib/resources/endpoints.dart
@@ -1,6 +1,11 @@
 class EndPoints {
 
-  static const String LOGIN = '';
+  static const String HOST = 'http://192.168.121.228:3112/';
+
+  static const String LOGIN = 'token_auth/obtain_pair/';
+
+  static const String PROFILES_CLOSED = 'api/placement_and_internship/profiles/';
+  static const String PROFILES_ALL = 'api/placement_and_internship/profiles/not_closed/';
 
   // Amidst the Trials on Dathomir
   static const String CALLING_STARSHIPS = 'https://swapi.co/api/starships'; 
