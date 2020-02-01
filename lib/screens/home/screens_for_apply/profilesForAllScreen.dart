e02d642fa753fe0927fd2d679571915141e1cb97
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-02-01 16:52:37 +0530

    applyPage: applay screen added with tabs

---
+++ lib/screens/home/screens_for_apply/profilesForAllScreen.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
+import 'package:placement/models/profilesModel.dart';
+import 'package:placement/resources/endpoints.dart';
+import 'package:placement/services/api_models/fetchService.dart';
+import 'package:placement/shared/loadingPage.dart';
+
+class ProfilesForAllPage extends StatefulWidget {
+  ProfilesForAllPage({Key key}) : super(key: key);
+
+  @override
+  _ProfilesForAllPageState createState() => _ProfilesForAllPageState();
+}
+
+class _ProfilesForAllPageState extends State<ProfilesForAllPage> {
+
+  var _fetch;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetch  = FetchService();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var  _width = MediaQuery.of(context).size.width;
+    return Container(
+      child: FutureBuilder(
+        future: _giveList(context),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if(snapshot.data == null) {
+            return LoadingPage();
+          }
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: snapshot.data.length,
+            itemBuilder: (BuildContext context, int index) {
+              String _date =
+                snapshot.data[index].applicationDeadline !=null ?
+                Jiffy(snapshot.data[index].applicationDeadline.toString()).yMMMd :
+                'Open';
+              return ListTile(
+                title: Text(
+                  snapshot.data[index].companyName,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                subtitle: Text(
+                  "Apply Before: " + _date,
+                  style: TextStyle(
+                    height: 1.85,
+                  ),
+                ),
+                onTap: () {
+                  print("tapped");
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Future<List<ProfilesModel>>  _giveList(BuildContext context) async {    
+    List<ProfilesModel> _profiles = [];
+    var _data = await _fetch.fetchDataService(EndPoints.HOST+EndPoints.PROFILES_ALL);
+    for(var p in _data) {
+      _profiles.add(ProfilesModel.fromJson(p));
+    }
+    return _profiles;
+  }
+}
