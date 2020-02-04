eb65c61a6daf83710f94e74bf872a0ae98e9b48d
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-02-04 23:42:43 +0530

    resultDetailsCompanyWiseScreen: integration of CompanyWise detail result routes completed

--- lib/screens/home/screens_for_result_details/resultDetailsCompanyWise.dart
+++ lib/screens/home/screens_for_result_details/resultDetailsCompanyWise.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:placement/models/companyWiseStudentModel.dart';
+import 'package:placement/resources/endpoints.dart';
+import 'package:placement/services/api_models/fetchService.dart';
+import 'package:placement/shared/loadingPage.dart';
 
 class ResultDetailsCompanyWise extends StatefulWidget {
-  final Map<String, String> args;
+  final String args;
   ResultDetailsCompanyWise({Key key, this.args}) : super(key: key);
 
   @override
@@ -9,14 +13,69 @@ class ResultDetailsCompanyWise extends StatefulWidget {
 }
 
 class _ResultDetailsCompanyWiseState extends State<ResultDetailsCompanyWise> {
+  
+  var _fetch;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetch = FetchService();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Container(
        child: Scaffold(
          appBar: AppBar(
-           title: Text("Branch"),
+           title: Text("Company Results"),
          ),
+         body: _companyResults(context),
        ),
     );
+  }
+
+  Widget _companyResults(BuildContext context) {
+    return FutureBuilder(
+      future: _futureOfResults(context),
+      builder: (context, snapshot) {
+        if(snapshot.data == null) {
+          return LoadingPage();
+        }
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Card(
+              margin: EdgeInsets.only(bottom: 1),
+              child: ListTile(
+                title: Text(
+                  snapshot.data[index].studentName,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                subtitle: Text(
+                  snapshot.data[index].studentBranchName,
+                  style: TextStyle(
+                    height: 1.85
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Future<List<CompantWiseStudentModel>> _futureOfResults(BuildContext context) async {
+    List<CompantWiseStudentModel> _results = [];
+    var _data = await _fetch.fetchDataService(
+      EndPoints.RESULTS_HOST + widget.args
+    );
+    for(var r in _data) {
+      _results.add(CompantWiseStudentModel.fromJson(r));
+    }
+    return _results;
   }
 }
