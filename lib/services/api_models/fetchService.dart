bad4c7e30d728e529ea2c061b5b322aa6dcb3ab1
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-02-04 23:44:49 +0530

    fetchService: add token refresh capability

--- lib/services/api_models/fetchService.dart
+++ lib/services/api_models/fetchService.dart
@@ -5,6 +5,7 @@ import 'package:http/http.dart' as http;
 
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:placement/resources/endpoints.dart';
 import 'package:placement/resources/strings.dart';
 import 'package:placement/services/auth/auth_service.dart';
 
@@ -19,25 +20,20 @@ class FetchService {
   }
 
   void initState() { 
-    _openEncryptedBox();
     _auth = AuthService();
   }
-  var _box;
 
   Future<dynamic>  fetchDataService(String endPoint) async {
     String _jsonData;
-    Map< String, String> _headers = {
-      'Authorization' : 'Bearer ' + _box.get('access', defaultValue: '') 
-    };
+    Map< String, String> _headers = _auth.fetchHeaderProvider(endPoint);
     try {
       var res = await http.get(
         endPoint, 
-        headers: _headers); 
-      if(res.statusCode == 200) {
+        headers: _headers);print(res.statusCode);
+      if(res.statusCode == 200) {print("inside 200");
         return json.decode(res.body);
       }
-      if(res.statusCode == 401) {
-        _auth.refreshToken();
+      if(res.statusCode == 401 && endPoint != EndPoints.HOST + EndPoints.LOGIN) {
         return fetchDataService(endPoint);
       }
       return -1;
@@ -45,11 +41,5 @@ class FetchService {
       print(e.toString());
       return null;
     }
-  }
-
-  _openEncryptedBox() async {
-      await Hive.initFlutter();
-      await Hive.openBox(Strings.AUTH_BOX);
-      _box = Hive.box(Strings.AUTH_BOX);
   }
 }
