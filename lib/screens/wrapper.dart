6c63faf28958de8949db6cca5c479ad6c5a7e017
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-06-25 13:15:22 +0530

    parse singelton instance of the dataprovider throughout

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -31,78 +31,9 @@ class _WrapperPageState extends State<WrapperPage> {
     var _size = MediaQuery.of(context).size;
     double _height = _size.height;
     double _width = _size.width;
-    //return _auth.authStateListener() ? HomePage() : Authenticate();
     return ChangeNotifierProvider(
-      create: (context) => DataProvider(),
+      create: (context) => data,
       child: _auth.authStateListener() ? HomePage() : Authenticate()
     );
-    // return Scaffold(
-    //   //
-    //   appBar: AppBar(
-    //     title: Text("test"),
-    //   ),
-    //   drawer: Drawer(
-    //     elevation: 8.0,
-    //     child: menu(context, _height, _width),
-    //   ),
-    //   body: Container(
-    //     color: Colors.red,
-    //     height: 200,
-    //     width: 200,
-    //   ),
-    // );
   }
-
-  Widget dashboard(BuildContext context, double _height, double _width) {
-    // return Container(
-    //   child: AnimatedPositioned(
-    //     top: _isCollapsed ? 0 : 0.2*_height,
-    //     bottom: _isCollapsed ? 0 : 0.2*_height,
-    //     right: _isCollapsed ? 0 : -0.1*_width,
-    //     left: _isCollapsed ? 0 : 0.5*_width,
-    //     duration: _duration,
-    //     // child: _auth.authStateListener() ? HomePage() : Authenticate(),
-    //     child: Material(
-    //       animationDuration: _duration,
-    //       borderRadius: BorderRadius.all(Radius.circular(40.0)),
-    //       elevation: 8.0,
-    //       child: Scaffold(
-    //         appBar: AppBar(title: Text("test"),),
-    //         body: Container(
-    //           color: Colors.red,
-    //           width: 200,
-    //           height: 200,
-    //         ),
-    //       )
-    //     ),
-    //   ),
-    // );
-    return Container(
-      child: AnimatedPositioned(
-        duration: _duration,
-        // top: _isCollapsed ? 0 : 0.2*_height,
-        // bottom: _isCollapsed ? 0 : 0.2*_height,
-        // right: _isCollapsed ? 0 : -0.1*_width,
-        // left: _isCollapsed ? 0 : 0.5*_width,
-        child: Material(
-          animationDuration: _duration,
-          borderRadius: BorderRadius.all(Radius.circular(40.0)),
-          elevation: 8.0,
-          child: Container(
-            color: Colors.red,
-            child: Scaffold(
-              appBar: AppBar(title: Text("test"),),
-              body: Container(
-                color: Colors.blue,
-                height: 200,
-                width: 200,
-              ),
-            ),
-          ),
-        )
-      ),
-    );
-  }
-
-  
 }
