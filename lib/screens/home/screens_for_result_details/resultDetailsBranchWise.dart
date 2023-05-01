062606ca038fc3b7154d24c9974b1eeb9724e934
just-ary27 <aryan_r@ch.iitr.ac.in>
2023-05-01 11:50:48 +0530

    Branch and Company Result: Added Search Feature

--- lib/screens/home/screens_for_result_details/resultDetailsBranchWise.dart
+++ lib/screens/home/screens_for_result_details/resultDetailsBranchWise.dart
@@ -13,23 +13,109 @@ class ResultDetailsBranchWise extends StatefulWidget {
       _ResultDetailsBranchWiseState();
 }
 
-class _ResultDetailsBranchWiseState extends State<ResultDetailsBranchWise> {
+class _ResultDetailsBranchWiseState extends State<ResultDetailsBranchWise>
+    with SingleTickerProviderStateMixin {
   var _fetch;
+  List<BranchWiseStudentModel> _results;
+  List<BranchWiseStudentModel> _resultsBackup;
+  AnimationController animationController;
+  Animation<double> animation;
+  OverlayEntry overlayEntry;
 
   @override
   void initState() {
     super.initState();
     _fetch = FetchService();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    animation = CurveTween(curve: Curves.fastOutSlowIn).animate(
+      animationController,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Branch Results"),
+    return WillPopScope(
+      onWillPop: () async {
+        if (overlayEntry.mounted) {
+          await Future.delayed(Duration(milliseconds: 10)).whenComplete(
+            () => animationController.reverse(),
+          );
+          overlayEntry.remove();
+          return false;
+        }
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Branch Results"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                _showOverlay(context);
+              },
+              icon: Icon(
+                Icons.search_rounded,
+              ),
+            )
+          ],
+        ),
+        body: _branchResults(context),
       ),
-      body: _branchResults(context),
     );
+  }
+
+  void _showOverlay(BuildContext context) async {
+    OverlayState overlayState = Overlay.of(context);
+    overlayEntry = OverlayEntry(
+      builder: (context) {
+        return Positioned(
+          left: 0,
+          child: FadeTransition(
+            opacity: animation,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: kToolbarHeight + MediaQuery.of(context).viewPadding.top,
+              child: AppBar(
+                leading: IconButton(
+                  icon: Icon(Icons.arrow_back_rounded),
+                  onPressed: () async {
+                    await Future.delayed(Duration(milliseconds: 10))
+                        .whenComplete(
+                      () => animationController.reverse(),
+                    );
+                    animationController.reverse();
+                    overlayEntry.remove();
+                  },
+                ),
+                title: TextField(
+                  maxLines: 1,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  onChanged: _branchResultsFiltered,
+                  decoration: InputDecoration(
+                    hintText: "Search Name...",
+                    border: InputBorder.none,
+                    hintStyle: TextStyle(
+                      color: Colors.white.withOpacity(0.75),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+    animationController.addListener(() {
+      overlayState.setState(() {});
+    });
+    // inserting overlay entry
+    overlayState.insert(overlayEntry);
+    animationController.forward();
   }
 
   Widget _branchResults(BuildContext context) {
@@ -39,21 +125,24 @@ class _ResultDetailsBranchWiseState extends State<ResultDetailsBranchWise> {
         if (snapshot.data == null) {
           return LoadingPage();
         }
+        if (_results.isEmpty) {
+          return Center(child: Text("Looks there's no one with that name :("));
+        }
         return ListView.builder(
           shrinkWrap: true,
           padding: EdgeInsets.all(0),
-          itemCount: snapshot.data.length,
+          itemCount: _results.length,
           itemBuilder: (context, index) {
             return Card(
               margin: EdgeInsets.only(bottom: 1),
               elevation: 0.2,
               child: ListTile(
                 title: Text(
-                  snapshot.data[index].studentName,
+                  _results[index].studentName,
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
                 subtitle: Text(
-                  snapshot.data[index].companyName,
+                  _results[index].companyName,
                   style: TextStyle(height: 1.85),
                 ),
               ),
@@ -64,17 +153,34 @@ class _ResultDetailsBranchWiseState extends State<ResultDetailsBranchWise> {
     );
   }
 
-  Future<List<BranchWiseStudentModel>> _futureOfResults(
-      BuildContext context) async {
-    List<BranchWiseStudentModel> _results = [];
+  void _branchResultsFiltered(String keyword) {
+    setState(() {
+      if (keyword == "") {
+        _results = _resultsBackup;
+      } else {
+        _results = _results
+            .where((element) => element.studentName.contains(keyword))
+            .toList();
+      }
+    });
+  }
+
+  Future<String> _futureOfResults(BuildContext context) async {
+    if (_results != null && _results.length >= 0) return "Success!";
+
+    List<BranchWiseStudentModel> _studentResults = [];
     var _data = await _fetch
         .fetchDataService(EndPoints.RESULTS_HOST + widget.args['url']);
     for (var r in _data) {
-      _results.add(BranchWiseStudentModel.fromJson(r));
+      _studentResults.add(BranchWiseStudentModel.fromJson(r));
     }
     if (widget.args['sort'] == 1) {
-      _results.sort((a, b) => a.studentName.compareTo(b.studentName));
+      _studentResults.sort((a, b) => a.studentName.compareTo(b.studentName));
     }
-    return _results;
+
+    _results = _studentResults;
+    _resultsBackup = _results;
+
+    return "Success!";
   }
 }
--- lib/screens/home/screens_for_result_details/resultDetailsCompanyWise.dart
+++ lib/screens/home/screens_for_result_details/resultDetailsCompanyWise.dart
@@ -13,25 +13,110 @@ class ResultDetailsCompanyWise extends StatefulWidget {
       _ResultDetailsCompanyWiseState();
 }
 
-class _ResultDetailsCompanyWiseState extends State<ResultDetailsCompanyWise> {
+class _ResultDetailsCompanyWiseState extends State<ResultDetailsCompanyWise>
+    with SingleTickerProviderStateMixin {
   var _fetch;
+  List<CompantWiseStudentModel> _results;
+  List<CompantWiseStudentModel> _resultsBackup;
+  AnimationController animationController;
+  Animation<double> animation;
+  OverlayEntry overlayEntry;
 
   @override
   void initState() {
     super.initState();
     _fetch = FetchService();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    animation = CurveTween(curve: Curves.fastOutSlowIn).animate(
+      animationController,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Company Results"),
+    return WillPopScope(
+      onWillPop: () async {
+        if (overlayEntry.mounted) {
+          await Future.delayed(Duration(milliseconds: 10)).whenComplete(
+            () => animationController.reverse(),
+          );
+          overlayEntry.remove();
+          return false;
+        }
+        return true;
+      },
+      child: Container(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Company Results"),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  _showOverlay(context);
+                },
+                icon: Icon(
+                  Icons.search_rounded,
+                ),
+              )
+            ],
+          ),
+          body: _companyResults(context),
         ),
-        body: _companyResults(context),
       ),
     );
+  }
+
+  void _showOverlay(BuildContext context) async {
+    OverlayState overlayState = Overlay.of(context);
+    overlayEntry = OverlayEntry(
+      builder: (context) {
+        return Positioned(
+          left: 0,
+          child: FadeTransition(
+            opacity: animation,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: kToolbarHeight + MediaQuery.of(context).viewPadding.top,
+              child: AppBar(
+                leading: IconButton(
+                  icon: Icon(Icons.arrow_back_rounded),
+                  onPressed: () async {
+                    await Future.delayed(Duration(milliseconds: 10))
+                        .whenComplete(
+                      () => animationController.reverse(),
+                    );
+                    overlayEntry.remove();
+                  },
+                ),
+                title: TextField(
+                  maxLines: 1,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  onChanged: _companyResultsFiltered,
+                  decoration: InputDecoration(
+                    hintText: "Search Name...",
+                    border: InputBorder.none,
+                    hintStyle: TextStyle(
+                      color: Colors.white.withOpacity(0.75),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+    animationController.addListener(() {
+      overlayState.setState(() {});
+    });
+    // inserting overlay entry
+    overlayState.insert(overlayEntry);
+    animationController.forward();
   }
 
   Widget _companyResults(BuildContext context) {
@@ -41,8 +126,12 @@ class _ResultDetailsCompanyWiseState extends State<ResultDetailsCompanyWise> {
         if (snapshot.data == null) {
           return LoadingPage();
         }
+        if (_results.isEmpty) {
+          return Center(child: Text("Looks there's no one with that name :("));
+        }
+
         return ListView.builder(
-          itemCount: snapshot.data.length,
+          itemCount: _results.length,
           padding: EdgeInsets.all(0),
           shrinkWrap: true,
           itemBuilder: (context, index) {
@@ -51,7 +140,7 @@ class _ResultDetailsCompanyWiseState extends State<ResultDetailsCompanyWise> {
               elevation: 0.2,
               child: ListTile(
                 title: Text(
-                  snapshot.data[index].studentName,
+                  _results[index].studentName,
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
                 subtitle: Container(
@@ -61,11 +150,11 @@ class _ResultDetailsCompanyWiseState extends State<ResultDetailsCompanyWise> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       Text(
-                        snapshot.data[index].studentBranchName,
+                        _results[index].studentBranchName,
                         style: TextStyle(height: 1.6),
                       ),
                       Text(
-                        "Accepted: " + snapshot.data[index].hasAccepted,
+                        "Accepted: " + _results[index].hasAccepted,
                         style: TextStyle(height: 1.6),
                       ),
                     ],
@@ -79,17 +168,33 @@ class _ResultDetailsCompanyWiseState extends State<ResultDetailsCompanyWise> {
     );
   }
 
-  Future<List<CompantWiseStudentModel>> _futureOfResults(
-      BuildContext context) async {
-    List<CompantWiseStudentModel> _results = [];
+  void _companyResultsFiltered(String keyword) {
+    setState(() {
+      if (keyword == "") {
+        _results = _resultsBackup;
+      } else {
+        _results = _results
+            .where((element) => element.studentName.contains(keyword))
+            .toList();
+      }
+    });
+  }
+
+  Future<String> _futureOfResults(BuildContext context) async {
+    if (_results != null && _results.length >= 0) return "Success!";
+
+    List<CompantWiseStudentModel> _studentResults = [];
     var _data = await _fetch
         .fetchDataService(EndPoints.RESULTS_HOST + widget.args['url']);
     for (var r in _data) {
-      _results.add(CompantWiseStudentModel.fromJson(r));
+      _studentResults.add(CompantWiseStudentModel.fromJson(r));
     }
     if (widget.args['sort'] == 1) {
-      _results.sort((a, b) => a.studentName.compareTo(b.studentName));
+      _studentResults.sort((a, b) => a.studentName.compareTo(b.studentName));
     }
-    return _results;
+    _results = _studentResults;
+    _resultsBackup = _results;
+
+    return "Success!";
   }
 }
